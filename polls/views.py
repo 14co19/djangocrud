b2d6d98256a0995225263e454828ed16a7ee4f91
14co19 <14co19@gmail.com>
2025-01-22 20:07:10 +0530

    added new template and views to display the ques

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Question
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    question = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template("polls/index.html")
+    data = {
+        "latest_question": question,
+    }
+    return render(request, 'polls/index.html', data)
+    # return HttpResponse(template.render(data, request))
+
+def detail(request, question_id):
+    ques = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {"question": ques})
+
+def result(request, question_id):
+    response = "You're looking at the result of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting for question %s." %question_id)
